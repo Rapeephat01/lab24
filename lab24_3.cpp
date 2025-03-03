8115419426882035a2c6daf865c082ebbf2531c6
Rapeephat01 <watdee00@GMAIL.COM>
2025-03-04 01:05:59 +0700

    Update lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 //Write function count() here.
 
+
+int count(int arr[], int size) {
+    set<int> unique_numbers(arr, arr + size); // ใช้ set เพื่อเก็บค่าที่ไม่ซ้ำกัน
+    return unique_numbers.size(); // คืนค่าจำนวนสมาชิกที่ไม่ซ้ำกัน
+}
+
+
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
